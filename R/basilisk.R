402f1a8d13dba52d7682729788e0f4573943ec79
RoseHuRan <huran@ucla.edu>
2023-10-18 12:56:03 -0500

    tensorflow

--- R/basilisk.R
+++ R/basilisk.R
@@ -3,5 +3,5 @@ my_env <- BasiliskEnvironment(envname="new_env",
                               packages=c("python==3.8",
                                          "numpy==1.23.4", 
                                          "scipy==1.8.0",
-                                         "tensorflow==2.14.0")
+                                         "tensorflow==2.10.0")
                               )
